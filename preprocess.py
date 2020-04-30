d459c53efad937e99792f451e2d75e055e8e4e8b
anilkumarravuru <anilkumar@anils-air.attlocal.net>
2020-04-30 06:28:01 -0400

    Actual model with parametter tuning

--- preprocess.py
+++ preprocess.py
@@ -3,6 +3,7 @@
 import pandas as pd
 
 df = pd.read_json('../Project/fb_data.json')
+print(df.count())
 print(df.columns)
 useful_cols = ['fb_like', 'fb_angry', 'fb_thankful', 'fb_haha', 'description', 'fb_sad', 'message', 'fb_wow', 'fb_love']
 xdf = df[useful_cols]
